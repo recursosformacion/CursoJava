a6e409b40ed7de38955d1be24fe03fc6b907b3cb
Miguel Garcia <migarcia@dopc.com>
2016-09-02 18:12:08 +0200

    Change-Id: Id7dfd258c64ca2e89b765e27d97e21cb5f158da1

--- GWT-Manejador_misma_clase/src/rf/manejadores/separada/client/GwtEventos.java
+++ GWT-Manejador_misma_clase/src/rf/manejadores/separada/client/GwtEventos.java
@@ -1,38 +1,52 @@
 package rf.manejadores.separada.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 
-public class GwtEventos implements EntryPoint, IGwtAcciones {
+public class GwtEventos implements EntryPoint, KeyUpHandler {
 	
 	private TextBox textField;
 	private HTML resultArea;
 	
+	
 	@Override
 	public void onModuleLoad() {
 		textField = new TextBox();
-		textField.addKeyUpHandler(new GestorKeyUp(this));
+		textField.addKeyUpHandler(this);
 		resultArea = new HTML("<i>Aqui aparecera el resultado</i>");
-		RootPanel.get().addStyleName("tan");
+		//RootPanel.get().addStyleName("tan");
 		RootPanel.get("textfieldID").add(textField);
 		RootPanel.get("resultID").add(resultArea);
 	}
 
-	@Override
 	public void setResults(String html) {
 		resultArea.setHTML(html);		
 	}
 
-	@Override
 	public void ponerFondoRojo() {
 		RootPanel.get().addStyleName("rojo");		
 	}
 
-	@Override
 	public void ponerFondoNormal() {
 		RootPanel.get().removeStyleName("rojo");		
 	}
 
+	public void onKeyUp(KeyUpEvent event) {
+		if (event.getSource() instanceof TextBox){
+			TextBox txt = (TextBox) event.getSource();
+			String text = txt.getText();
+			if (text.equalsIgnoreCase("rojo")){
+				setResults("<span class='correcto'>De Acuerdo!</span>");
+				ponerFondoRojo();
+			} else {
+				setResults(text);
+				ponerFondoNormal();
+			}
+		}
+
+	}
 }
